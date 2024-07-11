4f97e7f68ea513e9547fb6bedd25b0996cf53cbb
PRABHNOOR11 <official.prabhnoor3@gmail.com>
2024-07-11 13:28:58 +0000

    container commit done

---
+++ basic/variable.js
@@ -0,0 +1,10 @@
+let accountId= 4444
+const accountEmail="pta@google.com"
+var accountPassword ="777"
+accountCity="delhi"
+
+accountId=444
+accountPassword="000"
+accountCity= "goa"
+
+console.table([accountId, accountPassword, accountCity]);
